6dc0c68f4a949d013460066d22311de9b74d8fed
Guilherme <guilherme@gsbrownie.com.br>
2026-07-25 17:50:20 -0300

    chore: configura URL do Google Apps Script

--- config.js
+++ config.js
@@ -9,4 +9,4 @@
  *   const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/ABCDE12345/exec';
  */
 
-const APPS_SCRIPT_URL = ''; // ← COLE A URL DO WEB APP AQUI
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyQGM-jGByEkSr1_E8bb9TE8Fg201kCS5XS8UG9YYUo4Ut6F8PhIZyx1FIhn8afKkQ/exec';
